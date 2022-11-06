f2ad751d48de1c15957f68473c8765bb2a98f9b0
TNTErick-Laptop <30978340+TNTErick@users.noreply.github.com>
2022-11-06 19:07:24 +0800

    updating range

--- compatibility/cppvers.h
+++ compatibility/cppvers.h
@@ -1,7 +1,7 @@
 /*
  * This file helps with detection of C++ versions.
  * 
- * The macro __cplusplus should be set to 199711L, 201103L, 201402L, 201703L, 202002L for C++97, C++11, C++14, C++17, and C++20 respectively.
+ * The macro __cplusplus should be set to 199711L, 201103L, 201402L, 201703L, 202002L for C++98, C++11, C++14, C++17, and C++20 respectively.
  * However in MSVC, the macro is defined to 199711L regardless of C++versions. The corresponding value is defined in _MSVC_LANG.
  * 
  * Plus, some older version of compiler defines things differently, e.g. gcc 6.3.0 when compiling C++17, __cplusplus is defined to 201500L.
@@ -35,6 +35,9 @@
 #define cpp17 1
 #if __cplusplus > 202000L
 #define cpp20 1
+#if __cplusplus > 202300L
+#define cpp23 1
+#endif//202300
 #endif//202000
 #endif//201499
 #endif//201400
--- py/list.hpp
+++ py/list.hpp
@@ -4,28 +4,11 @@
 
 #ifndef __PY_LIST_
 #define __PY_LIST_
+#include"data-structure/node-sing-link.h"
 
 namespace py
 {
-	class object {};
 
-	template<typename T>
-	struct __py_list_node: T
-	{
-	public:
-		__py_list_node(__py_list_node* previous, ...)
-		{
-			//TODO: learn ellipsis in C++11
-		}
-		__py_list_node* previous = nullptr;
-		__py_list_node* next = nullptr;
-	};
-
-	template<typename T>
-	__py_list_node* __py_make_list_node(T vlaue)
-	{
-		auto tmp = new __py_list_node<T>(value);
-	}
 }
 
 #endif//__PY_LIST_
